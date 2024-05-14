298f80b0c9c9302c939c741a44a879af1a977ee2
faridev18 <zfarihane@gmail.com>
2024-05-14 18:36:14 +0100

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,7 @@
 @extends('layout.app')
 @section('contenu')
     <h1>Welcome </h1>
-    <div class="card card-success">
-        <div class="card-header">
-            <h3 class="card-title"> Accueil</h3>
-        </div>
-
-
-
-
-    </div>
-    <div class="card">
-
-        <div class="card-header">
-
-            <h3 class="card-title">Admin</h3>
-        </div>
-        <!-- /.card-header -->
-        <div class="card-body">
-
-        </div>
-        <!-- /.card-body -->
-    </div>
+    
+    
 
 @endsection
